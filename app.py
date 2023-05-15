4d4c91d32f9f31f15905deafc613e49a2786d1ff
Ankitha K <92176716+Ankitha-K21@users.noreply.github.com>
2023-05-15 22:09:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,39 +3,48 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
-# import pylance
+import sqlite3
+
 app = Flask(__name__)
 
 app.secret_key = 'your secret key'
- 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'A#'  ###
-app.config['MYSQL_DB'] = 'userlogin'
+
+def db_connection():
+    conn=None
+    try:
+        conn=sqlite3.connect('pin.sqlite')
+    except sqlite3.Error as e:
+        print(e)
+    return conn
 
 @app.route("/")
-@app.route("/login")
-@app.route('/home', methods =['GET', 'POST'])
 def home():
-    msg = ''
-    if request.method == 'POST' and 'atmpin' in request.form:
-        atmpin = request.form['atmpin']
-        cursor = MySQLError.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE atmpin = % d', (atmpin ))
-        account = cursor.fetchone()
-        if account:
-            session['loggedin'] = True
-            session['atmpin'] = account['atmpin']
-            msg = 'Logged in successfully !'
-            return render_template('bank_or_vote.html',msg=msg)
-        else:
-            msg = 'Incorrect pin'
-    return render_template('pin_entry.html',msg=msg)
-   
+    return render_template('pin_entry.html')
+
+@app.route('/login', methods =['POST','GET'])
+def login():
+    conn=db_connection()
+    card_number = request.form['card_number']
+    atm_pin = request.form['atm_pin']
+    
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE card_number = ? AND atm_pin = ?', (card_number, atm_pin))
+    conn.commit()
+
+    if cursor.fetchone() is not None:
+        return redirect(url_for('bank_or_vote'))
+    else:
+        error = 'Invalid card number or ATM pin. Please try again.'
+        return render_template('pin_entry.html', error=error)
+
+@app.route("/bank_or_vote")
+def bank_or_vote():
+    return render_template('bank_or_vote.html')
+
 @app.route("/vote")
 def vote():
-    return render_template('bank_or_vote.html')
- 
+    return render_template('vote.html')
+
 if __name__=="__main__":
     app.run(debug=True)
 
